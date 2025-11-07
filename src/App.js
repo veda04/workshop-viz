38846beebd6f0f2c7117bfdb39df4fe7e9381aed
veda04 <vedasalkar04@gmail.com>
2025-11-07 15:13:14 +0000

    RESTRUCTURING: Deleted unwanted files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
-import MachineSummary from './components/MachineSummary';
-import SensorDetails from './components/SensorDetails';
-import Analytics from './components/Analytics';
-import InfluxDBTest from './components/InfluxDBTest';
+import MachineSummary from './components/dashboard/MachineSummary';
 import './App.css';
 
 function App() {
@@ -13,11 +9,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<MachineSummary replace />} />
-          <Route path="/influxdb-test" element={<InfluxDBTest />} />
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
           <Route path="/machine-summary" element={<MachineSummary />} />
-          <Route path="/sensors" element={<SensorDetails />} />
-          <Route path="/analytics" element={<Analytics />} />
         </Routes>
       </div>
     </Router>
